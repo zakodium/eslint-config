e35e04df92f8a96eba0bfa6328deea6c318cdf4c
Daniel Kostro <kostro.d@gmail.com>
2020-11-19 09:57:40 +0100

    feat: add config for js create react app (#5)

---
+++ react-app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  extends: ['eslint-config-cheminfo-react'],
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'no-unused-vars': 'warn',
+  },
+};
