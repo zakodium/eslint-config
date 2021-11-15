6924e406501968eb4220a2a1a2ab1292d381352a
Michaël Zasso <targos@protonmail.com>
2021-11-15 09:06:22 +0100

    feat: add adonis-base config
    Useful when the typescript config is already extended and overridden
    in a parent folder.

---
+++ adonis-base.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    'import/no-unresolved': 'off',
+    'import/default': 'off',
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc' },
+        pathGroups: [
+          {
+            pattern: '{@ioc:*,@ioc:*/**}',
+            group: 'external',
+            position: 'after',
+          },
+          { pattern: 'Contracts/**', group: 'internal', position: 'before' },
+          { pattern: 'App/**', group: 'internal', position: 'before' },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+      },
+    ],
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-parameter-properties': 'off',
+  },
+};
--- adonis.js
+++ adonis.js
@@ -1,37 +1,9 @@
 'use strict';
 
 module.exports = {
-  extends: 'eslint-config-cheminfo-typescript',
-  rules: {
-    'import/no-unresolved': 'off',
-    'import/default': 'off',
-    'import/order': [
-      'warn',
-      {
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          'index',
-        ],
-        'newlines-between': 'always',
-        alphabetize: { order: 'asc' },
-        pathGroups: [
-          {
-            pattern: '{@ioc:*,@ioc:*/**}',
-            group: 'external',
-            position: 'after',
-          },
-          { pattern: 'Contracts/**', group: 'internal', position: 'before' },
-          { pattern: 'App/**', group: 'internal', position: 'before' },
-        ],
-        pathGroupsExcludedImportTypes: ['builtin'],
-      },
-    ],
-    '@typescript-eslint/explicit-member-accessibility': 'off',
-    '@typescript-eslint/no-floating-promises': 'off',
-    '@typescript-eslint/no-parameter-properties': 'off',
-  },
+  extends: [
+    'eslint-config-cheminfo-typescript',
+    './adonis-base.js',
+  ],
 };
+
